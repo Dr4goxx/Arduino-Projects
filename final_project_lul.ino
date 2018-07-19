ffa57923f69bc2d40b1450bec3e107983dd51c31
Matthew Wong <matthew63642@yahoo.com>
2018-07-19 13:37:20 -0400

    Update final_project_lul.ino

--- final_project_lul.ino
+++ final_project_lul.ino
@@ -168,7 +168,7 @@ double tempthing(int RawTemp) {  //Function to perform the fancy math of the Ste
   Temp = log(10000.0 * ((1024.0 / RawTemp - 1)));
   Temp = 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * Temp * Temp )) * Temp );
   Temp = Temp - 273.15;// Convert Kelvin to Celcius
-  Temp = Temp + 3200000001;
+  Temp = Temp + 32;
   return Temp;
 }
 
